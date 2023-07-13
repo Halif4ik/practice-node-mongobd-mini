3aa0c07a7816469373a55272cc2d5d6eff094b8c
DimaGrimach <chiki.briki2@gmail.com>
2023-07-13 18:59:56 +0300

    before divede b Route

--- index.js
+++ index.js
@@ -12,13 +12,27 @@ exprApp.engine('hbs',hbs.engine);
 exprApp.set('view engine', 'hbs');
 exprApp.set('views','views');
 
+exprApp.use(express.static('public'))
 exprApp.get('/', (req , res) => {
-    res.render('index')
+    res.render('index',{
+        title:"Main page",
+        isHome: true,
+    })
 });
 
 exprApp.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about',{
+        title:"About",
+        isAbout: true,
+    })
 });
+exprApp.get('/add', (req, res) => {
+    res.render('add',{
+        title:"Java script",
+        isJS: true,
+    })
+});
+
 /*start express App*/
 exprApp.listen(port, () => {
     console.log(`Example MYapp listening on port ${port}`)
