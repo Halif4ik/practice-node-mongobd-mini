0d646aa057ba22172039624f673fa7aca9d6d377
DimaGrimach <chiki.briki2@gmail.com>
2023-08-19 18:34:30 +0300

    deleted constants from git

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 module.exports={
      mongoURL: 'mongodb+srv://chikibriki2:KQoDNhuWCigEleSS@cluster1.vhd57va.mongodb.net/Exchange_of_developers',
-     SEND_GRID_API_KEY: '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',
+     SEND_GRID_API_KEY: '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',
      HOST_EMAIL: 'chiki-briki2@ya.ru',
      BASE_URL: 'http://grymachdmitro.ddns.net/',
 }
