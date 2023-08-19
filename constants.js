38fbac8677bf23c00663f2575fc83be42d56bcdd
DimaGrimach <chiki.briki2@gmail.com>
2023-08-19 18:36:23 +0300

    test

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 module.exports={
      mongoURL: 'mongodb+srv://chikibriki2:KQoDNhuWCigEleSS@cluster1.vhd57va.mongodb.net/Exchange_of_developers',
      SEND_GRID_API_KEY: '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',
+     ff22: 'S3333vM',
      HOST_EMAIL: 'chiki-briki2@ya.ru',
      BASE_URL: 'http://grymachdmitro.ddns.net/',
 }
